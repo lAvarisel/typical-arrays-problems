17ce567913dfe62a034e5ebc96a4ebb9e5db312a
Dmitry Melnikov <dmitry.melni@gmail.com>
2020-10-03 14:19:02 +0300

    feat: implement 3 functions: max, min, and avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (Array.isArray(array) === false || array.length === 0) {
+    return 0;
+  }
+  let min = array[0];
+  for (i = 1; i < array.length; ++i) {
+      if (array[i] < min) min = array[i];
+  }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (Array.isArray(array) === false || array.length === 0) {
+    return 0;
+  }
+  let max = array[0];
+  for (i = 1; i < array.length; ++i) {
+      if (array[i] > max) max = array[i];
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (Array.isArray(array) === false || array.length === 0) {
+    return 0;
+  }
+  return array.reduce((a, b) => (a + b)) / array.length;
 }
